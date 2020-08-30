19483d6c67e5bd835b010c141caffc1de84a70a8
Figo <figoaranta@gmail.com>
2020-08-30 13:30:10 +0700

    added prefix

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::prefix('v1')->group(function(){
+Route::prefix('backend3/v1')->group(function(){
 	Route::apiResource('hutangs','Api\v1\HutangController');
 	Route::apiResource('piutangs','Api\v1\PiutangController');
 });
